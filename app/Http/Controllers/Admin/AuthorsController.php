2634fc474cca7fc1b65d523967409fb8301570b6
”KeisukeShishido” <aya.kotori0723@gmail.com>
2020-03-13 07:18:59 +0000

    タトレントと著者の作成機能

--- app/Http/Controllers/Admin/AuthorsController.php
+++ app/Http/Controllers/Admin/AuthorsController.php
@@ -4,8 +4,36 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Author;
 
 class AuthorsController extends Controller
 {
-    //
+     /**
+     * add
+     * 
+     * GETでブラウザでからアクセスされる
+     * 新規作成用のviewを表示する。
+    */
+    public function add()
+    {
+        return view('admin.authors.create');
+    }
+
+    /**
+     * create
+     * 
+     * POSTされた内容をDBに新規保存する
+    */
+    public function create(Request $request) {
+      $this->validate($request, Author::$rules);
+
+      $author = new Author;
+      $form = $request->all();
+
+      // データベースに保存する
+      $author->fill($form);
+      $author->save();
+
+      return redirect('admin/authors/add');
+    }
 }
--- app/Http/Controllers/Admin/TalentsController.php
+++ app/Http/Controllers/Admin/TalentsController.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Talent;
+use Storage;
 
 class TalentsController extends Controller
 {
-    //
+    /**
+     * add
+     * 
+     * GETでブラウザでからアクセスされる
+     * 新規作成用のviewを表示する。
+    */
+    public function add()
+    {
+        return view('admin.talents.create');
+    }
+
+    /**
+     * create
+     * 
+     * POSTされた内容をDBに新規保存する
+    */
+    public function create(Request $request) {
+      $this->validate($request, Talent::$rules);
+
+      $talent = new Talent;
+      $form = $request->all();
+      
+       // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $talent->image_path = Storage::disk('s3')->url($path);
+      } else {
+        $talent->image_path = null;
+      }
+      unset($form['image']);
+      // データベースに保存する
+      $talent->fill($form);
+      $talent->save();
+
+      return redirect('admin/talents/add');
+    }
 }
